9baa337e1bef6bde0ff285dc1660b29d5a4f4c95
HHS <carnad37@gmail.com>
2019-02-12 00:58:26 +0900

    20190211
    집에서

--- 190129/Test07.py
+++ 190129/Test07.py
@@ -1,75 +1,70 @@
-def loginSystem(userDic):
-    # for i in range(0,5,1): #로 반복횟수에 제한을 둘수있다.
-    while True:
-        #idChecker만 불러옴로 id만 체크
-        id_input = idChecker(userDic)
-        pw_input = input("비밀번호를 입력해주세요: ")
-        if(pw_input == userDic[id_input]):
+#총 3가지 메뉴.
+#로그인을 했을때만 2,3 가능.
+#1,2,3이 아닐 시엔 다시 입력.
+
+#함수제작
+#매개변수로 id를 전달해서 id가 존재하는지 리턴하는 함수 작성
+#있을시엔 1을 리턴. 없을 시엔 0을 리턴.
+#매개변수로 id를 전달해서 pw가 맞는지 리턴하는 함수 작성
+#매개변수로 id를 전달해서 pw가 맞는지 리턴하는 함수 작성
+
+def idChecker(id,pw):
+    global user_id_List
+    global user_pw_List
+    check_res = 0
+    #check_res가 0일시 id가 없다.
+    #check_res가 1일시 id는 있는데 비밀번호가 없다.
+    #check_res가 2일시 로그인가능.
+    for i in range(0,len(user_id_List),1):
+        if(id == user_id_List[i]):
+            check_res = 1
             break
-        else:
-            #userDIc에 없다면
-            print("패스워드가 잘못입력되었습니다." + "\n")
-    print(id_input + "님, 환영합니다.")
-    #해당 아이디를 로그인상태(True)로 바꾸기위해 리턴받는다.
-    return id_input
+    if(check_res == 1):
+        for i in range(0,len(user_pw_List),1):
+            if(pw == user_pw_List[i]):
+                check_res = 2
+                break
+    return check_res
 
-#login딕셔너리의 키값 체크
-def idChecker(userDic):
-    while True:
-        id_input = input("아이디를 입력해주세요: ")
-        check = id_input in userDic
-        if (check):
-            break
-        print("존재하지 않는 아이디입니다." + "\n")
-        #내부에서 체크한 id를 외부에서도 사용하기위해서 리턴.
-    return id_input
+def loginChecker(id):
+    global login
+    check = login[id]
+    if(check):
+        print(id+"님은 로그인 중입니다.")
+    else:
+        print(id+"님은 로그아웃 상태입니다.")
 
-# id, password 데이터
-user = {"apple": "orange", "chair": "desk", "mouse": "keyboard", "person": "pocket"}
-point = {"apple": 10000, "chair": 2000, "mouse": 700, "person": 70}
 
-#login 딕셔너리 만들기, 기본값 False
+#1~3까지의 숫자만 입력가능.
+
+#id, password 데이터
+user = {"apple":"orange","chair":"desk","mouse":"keyboard","person":"pocket"}
 user_id_List = list(user.keys())
+user_pw_List = list(user.values())
 login = {}
-for i in range(0, len(user_id_List), 1):
-    login[user_id_List[i]] = False
+for i in range(0,len(user_id_List),1):
+    login[user_id_List[i]]=False
 
 while True:
     # 메뉴제작
     print("1. 로그인")
     print("2. 사용자 로그인 여부 조회")
     print("3. 포인트 조회")
-    print("4. 시스템 종료")
     selectMenu = int(input(">"))
     if (selectMenu == 1):
-        #user딕셔너리만 넣어준다.
-        #로그인 시스템내부에서 id입력체크 pw체크가 모두 이루워진다.
-        id_input = loginSystem(user)
-        #
-        login[id_input] = True
+        # 아이디와 비밀번호를 입력받는다.
+        id_input = input("아이디를 입력해주세요: ")
+        pw_input = input("비밀번호를 입력해주세요: ")
+        check_res = idChecker(id_input, pw_input)
+        if (check_res == 0):
+            print("ID가 존재하지 않습니다.")
+        elif (check_res == 1):
+            print("패스워드가 잘못입력되었습니다.")
+        else:
+            print(id_input + "님, 환영합니다.")
+            login[id_input] = True
     elif (selectMenu == 2):
         # 사용자 로그인 정보확인
-        id_input = idChecker(login)
-        if (login[id_input]):
-            print(id_input + "님은 로그인 중입니다.")
-        else:
-            print(id_input + "님은 로그아웃 상태입니다.")
-    elif (selectMenu == 3):
-        # loginChecker로 login딕셔너리에 id존재를 확인
-        id_input = idChecker(login)
-        # 로그인 상태인지 아닌지를 확인
-        if (login[id_input]):
-            res_point = point[id_input]
-            print(id_input + "님이 소유하신 포인트는", res_point, "입니다")
-        else:
-            print(id_input + "님은 로그아웃 상태입니다.")
-            print("먼저 로그인을 해주십시오")
-            id_input = loginSystem(user)
-            login[id_input] = True
-    elif(selectMenu == 4):
-        print("시스템을 종료합니다.")
-        break
-    else:
-        print("잘못된 입력입니다.")
+        id_input = input("아이디를 입력해주세요: ")
+        loginChecker(id_input)
 
-    print("\n" + "=" * 30 + "\n")
--- 190208/190209.py
+++ 190208/190209.py
@@ -1,7 +1,7 @@
 #이름이 입력됬을때 각 라인의 첫번쨰 요소와 비교.
 
 def student_Data(path_file):
-    #첫번째 요소는 Key, 이후요소는 튜플로 묶어서 valuse로 만들기.
+    #데이터를 리스트로 만들어주는 함수
     readfile = open(path_file,'r')
     checkList = []
     sumTuple = ()
@@ -16,20 +16,54 @@ def student_Data(path_file):
     return save_List
 
 def array_List(aList):
+    #행과 열을 바꿔주는 함수
     sum_List = []
     for i in aList[0]:
         array_check = [i]
         sum_List.append(array_check)
-    for j in
-    print(sum_List)
+    for i in range(0,len(sum_List),1):
+        for j in range(1,len(aList),1):
+            sum_List[i].append(aList[j][i])
     return sum_List
 
-path_file = "d:\\hhs2\\file\\subjectScore.csv"
-student_Dic = student_Data(path_file)
-#조건이 너무 심하다.
-auuuut = array_List(student_Dic)
+def calculate_Avg(sList):
+    #전과목 평균 리스트 제작
+    #sList[0]은 이름을 가지고있다.
+    #[1][1~끝]까지가 숫자다. 바꿔주자.
+    avg_List=["score_avg"]
+    sum_score = 0
+    max = len(sList[0])
+    print(max)
+    for i in range(1,max,1):
+        for j in range(1,len(sList),1):
+            sum_score+=int(sList[j][i])
+        sum_score=sum_score/(len(sList)-1)
+        avg_List.append(sum_score)
+        sum_score = 0
+    return avg_List
 
-print(student_Dic)
+def max_and_min_student(avgList,sList):
+    check_List = sorted(avgList[1:])
+    min_index = avgList.index(check_List[0])
+    max_index = avgList.index(check_List[len(check_List)-1])
+    min_student = sList[0][min_index]
+    max_student = sList[0][max_index]
+    return min_student,max_student
+
+path_file = "D:\\workspace\\files\\subScore.txt"
+student_pre_List = student_Data(path_file)
+student_List = array_List(student_pre_List)
+student_avg_List=calculate_Avg(student_List)
+(min_name,max_name)=max_and_min_student(student_avg_List,student_List)
+print(student_List)
+print(student_avg_List)
+print("평균 최저점의 학생은 %s이다"%min_name)
+print("평균 최고점의 학생은 %s이다"%max_name)
+
+
+#각 학생들의 평균 점수.
+#평균점수가 가장 높은 학생의 이름.
+#평균점수가 가장 낮은 학생의 이름.
 
 
 #원하는 과목을 name key의 val에서 서치. 그 값을 대입.
