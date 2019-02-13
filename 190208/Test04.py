27cc2a913b049222bb5c7bd93d57d47c5ddad793
carnad37 <carnad37@gmail.com>
2019-02-13 18:12:11 +0900

    20190213
    수업 종료

--- 190208/Test04.py
+++ 190208/Test04.py
@@ -1,17 +1,23 @@
-#이름이 입력됬을때 각 라인의 첫번쨰 요소와 비교.
-
 def student_Data(path_file):
     #데이터를 리스트로 만들어주는 함수
     readfile = open(path_file,'r')
     checkList = []
     sumTuple = ()
     save_List = []
-    for line  in readfile:
+    count = 0
+    for line in readfile:
         line = line.rstrip("\n")
         checkList = line.split(",")
-        # sumTuple = tuple(checkList[1:])
-        # saveDic[checkList[0]] = sumTuple
+        if count > 0:
+            indexCount = 0
+            for i in checkList[1:]:
+                i = int(i)
+                indexCount += 1
+                checkList[indexCount] = i
+        count += 1
+        #count는 행의 갯수. incount는 리스트 내의 인덱스
         save_List.append(checkList)
+    #여기서
     readfile.close()
     return save_List
 
@@ -26,6 +32,14 @@ def array_List(aList):
             sum_List[i].append(aList[j][i])
     return sum_List
 
+def calculate_pre_Avg(sList):
+    save_List = ["score_avg"]
+    for i in range(1,len(sList),1):
+        sum_score = sum(sList[i][1:])
+        sum_score = sum_score/(len(sList[0])-1)
+        save_List.append(sum_score)
+    return save_List
+
 def calculate_Avg(sList):
     #전과목 평균 리스트 제작
     #sList[0]은 이름을 가지고있다.
@@ -35,7 +49,7 @@ def calculate_Avg(sList):
     max = len(sList[0])
     for i in range(1,max,1):
         for j in range(1,len(sList),1):
-            sum_score+=int(sList[j][i])
+            sum_score+=sList[j][i]
         sum_score=sum_score/(len(sList)-1)
         avg_List.append(sum_score)
         sum_score = 0
@@ -59,18 +73,16 @@ def print_avg_scroe(avgList,sList):
 
 path_file = "d:\\hhs2\\file\\subjectScore.csv"
 student_pre_List = student_Data(path_file)
+print(student_pre_List)
+student_pre_avg_List = calculate_pre_Avg(student_pre_List)
+print(student_pre_avg_List)
 student_List = array_List(student_pre_List)
+print(student_List)
 student_avg_List=calculate_Avg(student_List)
-print_avg_scroe(student_avg_List,student_List)
+print(student_avg_List)
+# print_avg_scroe(student_avg_List,student_List)
 (min_name,max_name)=max_and_min_student(student_avg_List,student_List)
 
-print("")
-print("평균 최저점의 학생은 %s이다"%min_name)
-print("평균 최고점의 학생은 %s이다"%max_name)
-
-#각 학생들의 평균 점수.
-#평균점수가 가장 높은 학생의 이름.
-#평균점수가 가장 낮은 학생의 이름.
-
-#원하는 과목을 name key의 val에서 서치. 그 값을 대입.
-#각 학생의 평균 점수 구하기
+# print("")
+# print("평균 최저점의 학생은 %s이다"%min_name)
+# print("평균 최고점의 학생은 %s이다"%max_name)
--- 190212/Test01.py
+++ 190212/Test01.py
@@ -1,16 +1,46 @@
-#
+class StudentScoreData:
+    def __init__(self, file_path):
+        readfile = open(file_path,'r')
+        self.avg_List = []
+        self.name_List = []
+        count = 0
+        check_List = []
+        for line in readfile:
+            line = line.rstrip("\n")
+            check_List = line.split(",")
+            sum_num = 0
+            avg_num = 0
+            if count>0:
+                # for i in check_List[1:]:
+                #     i=int(i)
+                #     sum_num += i
+                sum_num = sum(list(map(int,check_List[1:])))
+                avg_num = sum_num / (len(check_List) - 1)
+                self.avg_List.append(avg_num)
+                self.name_List.append(check_List[0])
+            count += 1
+        readfile.close()
 
-def student_Data(path_file):
-    #데이터를 리스트로 만들어주는 함수
-    readfile = open(path_file,'r')
-    checkList = []
-    sumTuple = ()
-    save_List = []
-    for line  in readfile:
-        line = line.rstrip("\n")
-        checkList = line.split(",")
-        # sumTuple = tuple(checkList[1:])
-        # saveDic[checkList[0]] = sumTuple
-        save_List.append(checkList)
-    readfile.close()
-    return save_List
+    def score_sort_student(self):
+        sort_List = sorted(self.avg_List)
+        self.max_avg = sort_List[len(self.avg_List)-1]
+        self.min_avg = sort_List[0]
+        search = self.avg_List.index(self.max_avg)
+        self.max_name = self.name_List[search]
+        search = self.avg_List.index(self.min_avg)
+        self.min_name = self.name_List[search]
+
+    def print_avg_scroe(self):
+        # 모든 학생의 평균점수를 호출
+        for i in range(0, len(self.avg_List), 1):
+            name = self.name_List[i]
+            avg = self.avg_List[i]
+            print("%s의 평균점수는 %d이다" % (name, avg))
+
+ssd = StudentScoreData("d:\\hhs2\\file\\subjectScore.csv")
+ssd.print_avg_scroe()
+ssd.score_sort_student()
+print(ssd.avg_List)
+print("")
+print("가장 점수가 높은 학생의 이름은 %s이다."%ssd.max_name)
+print("가장 점수가 낮은 학생의 점수는 %d이다."%ssd.min_avg)
