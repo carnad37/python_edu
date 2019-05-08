8e93ba6343a7554c29d0b9750a452c2beb878446
carnad37 <46643023+carnad37@users.noreply.github.com>
2019-05-08 12:10:55 +0900

    Update Test03.py

--- 190124/Test03.py
+++ 190124/Test03.py
@@ -2,16 +2,16 @@
 #그걸 이용해서 3개를 비교 최대값을 뽑아내라
 
 
-def selectNum(num01,num02):
-    if(num01>num02):
-        num01=num02
+def selectNum(num01, num02):
+    if (num01 < num02):
+        num01 = num02
     return num01
 
 num01 = int(input("첫번째 수를 입력해주세요: "))
 num02 = int(input("두번째 수를 입력해주세요: "))
 num03 = int(input("세번째 수를 입력해주세요: "))
 
-min = selectNum(num01,num02)
-min = selectNum(min, num03)
+max = selectNum(num01,num02)
+max = selectNum(min, num03)
 
 print(min)
